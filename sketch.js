d138bdec3b672e27cb6e22791973319171a5c2b3
homika-kkk <73460421+homika-kkk@users.noreply.github.com>
2021-01-28 17:24:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,25 +35,7 @@ function draw() {
     edges=createEdgeSprites();
     ball.bounceOff(edges);
 
-    if(block1.isTouching(ball) && ball.bounceOff(block1)){
-        ball.shapeColor = rgb(0,0,255);
-        music.play();
-    }
-
-    if(block2.isTouching(ball)){
-        ball.shapeColor = rgb(255,128,0);
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        music.stop();
-    }
-
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = rgb(153,0,76);
-    }
-
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = rgb(0,100,0);
-    }
+   
 
     drawSprites();
 }
